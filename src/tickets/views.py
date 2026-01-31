c24c9456e810d763ec45be604b356d1db59ca06b
Matvey Tereshin <teriomta@gmail.com>
2026-01-31 20:04:07 +0300

    add RBAC: user/support/admin-access

--- src/tickets/views.py
+++ src/tickets/views.py
@@ -10,10 +10,23 @@ class TicketViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     filterset_fields = ["status"]
 
-
     def get_queryset(self):
-        # Пользователь видит только свои тикеты (идеально для стажёрского проекта)
-        return Ticket.objects.filter(owner=self.request.user).order_by("-created_at")
+        user = self.request.user
+        if user.is_superuser:
+            return Ticket.objects.all().order_by("-created_at")
+
+        if user.groups.filter(name__in=["support", "admin"]).exists():
+            return Ticket.objects.all().order_by("-created_at")
+
+        return Ticket.objects.filter(owner=user).order_by("-created_at")
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        if "status" in serializer.validated_data:
+            if not (user.is_superuser or user.groups.filter(name__in=["support", "admin"]).exists()):
+                from rest_framework.exceptions import PermissionDenied
+                raise PermissionDenied("Only support/admin can change status.")
+        serializer.save()
